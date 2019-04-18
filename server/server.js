4572d620a6e2f8a49ec8ad2d6bfaf6b8a4e6f567
Jordhan Carvalho <jordhan.rdz@gmail.com>
2019-04-18 16:46:08 -0300

    Port for heroku/not needed

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const buildPath = path.join(__dirname, '..', 'build');
 
 app.use(express.static(buildPath));
